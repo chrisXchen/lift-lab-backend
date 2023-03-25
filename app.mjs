b14c5c54ea72744632d194ffe1de1450f75ae742
Christopher Chen <chris.chen347@yahoo.com>
2023-03-24 22:33:33 -0400

    Basic app skeleton in app.mjs and packages

--- app.mjs
+++ app.mjs
@@ -1,9 +1,14 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url';
+import express from 'express';
+import path from 'path';
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-app.listen(process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Express App' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

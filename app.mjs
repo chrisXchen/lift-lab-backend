26105262984567a6f983c25d9254ad1c8bb03afb
Christopher Chen <chris.chen347@yahoo.com>
2023-04-06 21:21:02 -0400

    Quick fix to run server

--- app.mjs
+++ app.mjs
@@ -1,5 +1,9 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
